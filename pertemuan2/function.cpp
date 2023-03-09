9e1d2f70a97eec84a40d0116f450f1f63a63486f
aliffianilhamf <111202214155@mhs.dinus.ac.id>
2023-03-09 11:30:48 +0700

    Update function.cpp

--- pertemuan2/function.cpp
+++ pertemuan2/function.cpp
@@ -6,7 +6,7 @@ using namespace std;
 int jumlah(int x, int z);
 int kurang(int x, int y);
 int kali(int x, int y);
-float bagi(float x, float y);
+float bagi(int x, int y);
 
 // isikan default parameter disini
 // ketika parameter sudah diatur dengan nilai default ,
@@ -19,22 +19,27 @@ void tampil(int a, int b = 1, int c = 2);
 int main()
 {
     int a, b, hasilTambah, hasilKurang, hasilKali;
-    float hasilBagi;
+    // float hasilBagi;
 
     cout << "bilangan pertama : ";
     cin >> a;
     cout << "bilangan kedua : ";
     cin >> b;
 
-    hasilTambah = jumlah(a, b);
-    hasilKurang = kurang(a, b);
-    hasilKali = kali(a, b);
-    hasilBagi = bagi(a, b);
+    // hasilTambah = jumlah(a, b);
+    // hasilKurang = kurang(a, b);
+    // hasilKali = kali(a, b);
+    // hasilBagi = bagi(a, b);
 
-    cout << "hasil penjumlahanya adalah : " << hasilTambah << endl;
-    cout << "hasil pengurangannya adalah : " << hasilKurang << endl;
-    cout << "hasil perkaliannya adalah : " << hasilKali << endl;
-    cout << "hasil baginnya adalah : " << hasilBagi << endl;
+    // cout << "hasil penjumlahanya adalah : " << hasilTambah << endl;
+    // cout << "hasil pengurangannya adalah : " << hasilKurang << endl;
+    // cout << "hasil perkaliannya adalah : " << hasilKali << endl;
+    // cout << "hasil baginnya adalah : " << hasilBagi << endl;
+
+    cout << "hasil penjumlahanya adalah : " << jumlah(a, b) << endl;
+    cout << "hasil pengurangannya adalah : " << kurang(a, b) << endl;
+    cout << "hasil perkaliannya adalah : " << kali(a, b) << endl;
+    cout << "hasil baginnya adalah : " << bagi(a, b) << endl;
 
     cout << "default parameter function " << endl;
     tampil(2);
@@ -58,9 +63,11 @@ int kali(int x, int y)
 {
     return x * y;
 }
-float bagi(float x, float y)
+
+// fungsi bagi
+float bagi(int x, int y)
 {
-    return x / y;
+    return (float)x / y;
 }
 
 // default parameter
